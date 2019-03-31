ff0c25e3e6c9798d30d22d056cf812da7f959b0b
StreatCodes <matt.r.streatfield@gmail.com>
2019-03-31 18:20:46 +1100

    Fix resize

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -15,12 +15,13 @@ export class Editor extends Component {
 			editorPos = JSON.parse(editorPos);
 		}
 
-		this.dragging = null;
+		this.editorResizing = null;
+		this.editorMoving = null;
 		this.state = {
 			editorLeft: editorPos !== null ? editorPos.editorLeft : 0,
 			editorTop: editorPos !== null ? editorPos.editorTop : 0,
-			editorMaxWidth: editorPos !== null ? editorPos.editorMaxWidth : 300,
-			editorMaxHeight: editorPos !== null ? editorPos.editorMaxHeight : 300,
+			editorWidth: editorPos !== null ? editorPos.editorWidth : 300,
+			editorHeight: editorPos !== null ? editorPos.editorHeight : 300,
 			loadPercentage: 0.0,
 			selectedTab: 'pages'
 		};
@@ -53,21 +54,29 @@ export class Editor extends Component {
 
 	draggingOn(e) {
 		e.preventDefault();
-		this.dragging = e.target.parentElement;
+		this.editorMoving = document.getElementById('lotus-editor');
+	}
+
+	resizeOn(e) {
+		e.preventDefault();
+		this.editorResizing = document.getElementById('lotus-editor');
 	}
 
 	draggingOff() {
-		this.dragging = null;
+		this.editorMoving = null;
+		this.editorResizing = null;
 
 		window.localStorage.setItem('editorPos', JSON.stringify({
 			editorLeft: this.state.editorLeft,
-			editorTop: this.state.editorTop
+			editorTop: this.state.editorTop,
+			editorWidth: this.state.editorWidth,
+			editorHeight: this.state.editorHeight
 		}));
 	}
 
-	moveWindow(e) {
-		if(this.dragging !== null) {
-			const rect = this.dragging.getBoundingClientRect();
+	moveMouse(e) {
+		if(this.editorMoving !== null) {
+			const rect = this.editorMoving.getBoundingClientRect();
 			let left = rect.left + e.movementX;
 			let top = rect.top + e.movementY;
 	
@@ -90,16 +99,41 @@ export class Editor extends Component {
 				editorTop: top
 			});
 		}
+
+		if(this.editorResizing !== null) {
+			const rect = this.editorResizing.getBoundingClientRect();
+			let width = rect.width + e.movementX;
+			let height = rect.height + e.movementY;
+	
+			if(width < 300) {
+				width = 300;
+			}
+			if(width + rect.left > window.innerWidth) {
+				width = rect.width;
+			}
+	
+			if(height < 300) {
+				height = 300;
+			}
+			if(height + rect.top > window.innerHeight) {
+				height = rect.height;
+			}
+	
+			this.setState({
+				editorWidth: width,
+				editorHeight: height
+			});
+		}
 	}
 
 	componentWillMount() {
 		window.addEventListener('mouseup', this.draggingOff.bind(this));
-		window.addEventListener('mousemove', this.moveWindow.bind(this));
+		window.addEventListener('mousemove', this.moveMouse.bind(this));
 	}
 
 	componentWillUnmount() {
 		window.removeEventListener('mouseup', this.draggingOff);
-		window.removeEventListener('mousemove', this.moveWindow);
+		window.removeEventListener('mousemove', this.moveMouse);
 	}
 
 	render(props, state) {
@@ -120,8 +154,8 @@ export class Editor extends Component {
 			}
 		}
 
-		return html`<div class="lotus-editor"
-				style=${{left: state.editorLeft+'px', top: state.editorTop+'px', maxWidth: state.editorMaxWidth, maxHeight: state.editorMaxHeight}}>
+		return html`<div class="lotus-editor" id="lotus-editor"
+				style=${{left: state.editorLeft+'px', top: state.editorTop+'px', width: state.editorWidth, height: state.editorHeight}}>
 			<div class="lotus-header" onmousedown=${this.draggingOn.bind(this)}>
 				Editor
 			</div>
@@ -135,7 +169,8 @@ export class Editor extends Component {
 			</div>
 			<svg xmlns="http://www.w3.org/2000/svg" width="12" height="12"
 				viewBox="0 0 12 12" fill="none" stroke="#666666" stroke-width="1"
-				stroke-linecap="round" stroke-linejoin="round" class="lotus-resize">
+				stroke-linecap="round" stroke-linejoin="round" class="lotus-resize"
+				onmousedown=${this.resizeOn.bind(this)}>
 				<line x1="0" y1="10" x2="10" y2="0"/>
 				<line x1="4" y1="10" x2="10" y2="4"/>
 				<line x1="8" y1="10" x2="10" y2="8"/>
